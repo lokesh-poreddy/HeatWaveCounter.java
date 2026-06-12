b594fa339f866fdf4aa71666d566e4ba5453ce17
Lokesh Poreddy <your-email@example.com>
2026-06-12 23:39:42 +0530

    sucker

---
+++ hamming.java
@@ -0,0 +1,14 @@
+class hamming {
+    public int hammingWeight(int n) {
+        int oneBits = 0;
+        
+        
+        while (n > 0) {
+            oneBits += n % 2;
+            n = Math.floorDiv(n, 2);
+        }
+        
+        
+        return oneBits;
+    }
+}
